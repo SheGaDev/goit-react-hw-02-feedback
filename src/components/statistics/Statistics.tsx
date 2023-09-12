e1bd84f68bd2c56d55e196b7e249d61ea7c3799c
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-09-12 19:58:39 +0300

    Update Statistics.tsx

--- src/components/statistics/Statistics.tsx
+++ src/components/statistics/Statistics.tsx
@@ -1,5 +1,3 @@
-import Notification from '../notification/Notification';
-
 const Statistics = ({
   good,
   neutral,
@@ -14,19 +12,13 @@ const Statistics = ({
   positivePercentage: string;
 }) => {
   return (
-    <>
-      {total ? (
-        <div className='flex flex-col'>
-          <span>Good: {good}</span>
-          <span>Neutral: {neutral}</span>
-          <span>Bad: {bad}</span>
-          <span>Total: {total}</span>
-          <span>Positive feedback: {positivePercentage}</span>{' '}
-        </div>
-      ) : (
-        <Notification message='There is no feedback' />
-      )}
-    </>
+    <div className='flex flex-col'>
+      <span>Good: {good}</span>
+      <span>Neutral: {neutral}</span>
+      <span>Bad: {bad}</span>
+      <span>Total: {total}</span>
+      <span>Positive feedback: {positivePercentage}</span>{' '}
+    </div>
   );
 };
 
